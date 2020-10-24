328e78b77da5f3cfead109fcedb03d505c62e02c
George Ntoutsos <g.ntouts@outlook.com>
2020-10-24 22:57:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,15 @@ fbq('track', 'PageView');
 
 document.onreadystatechange = () => {
     if (document.readyState === 'interactive') {
+        console.log('begin');
         if (window.location.href.toString().includes('checkout/order-received')) {
             var totalValue = document.getElementsByClassName('woocommerce-order-overview__total')[0];
             totalValue = totalValue.getElementsByClassName('woocommerce-Price-amount')[0].innerText
             totalValue = totalValue.replace('€', '').trim();
             totalValue = parseFloat(totalValue);
             fbq('track', 'Purchase', { value: totalValue, currency: 'EUR' });
+            console.log(totalValue);
+
         }
     }
 };
