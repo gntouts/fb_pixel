b10c2860787b503024a638e1557a99b4722d2d9a
George Ntoutsos <g.ntouts@outlook.com>
2020-10-26 10:06:32 +0200

    Update and rename fb_second.js to fb_sale_track.js

---
+++ fb_sale_track.js
@@ -0,0 +1,17 @@
+function docReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+docReady(function() {
+    if (window.location.href.toString().includes('checkout/order-received')) {
+        let totalValue = document.getElementsByClassName('woocommerce-order-overview__total')[0];
+        totalValue = totalValue.getElementsByClassName('woocommerce-Price-amount')[0].innerText
+        totalValue = totalValue.replace('€', '').trim();
+        totalValue = parseFloat(totalValue);
+        fbq('track', 'Purchase', { value: totalValue, currency: 'EUR' });
+    };
+});
